49bd5643e0de99c4bbdcca6f742dcc76c0f29fda
tobiuo0203 <64510581+tobiuo0203@users.noreply.github.com>
2020-08-10 18:02:21 +0900

    first commit

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template
 #Flaskオブジェクトの生成
 app = Flask(__name__)
 
-#「/index」へアクセスがあった場合に、「index.html」を返す
+#「/」へアクセスがあった場合に、「index.html」を返す
 @app.route("/")
 def index():
     return render_template("index.html")
