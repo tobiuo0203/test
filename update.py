63e96ddb9cc410b434c64fbe4b0bbb691fef3410
tobiuo0203 <64510581+tobiuo0203@users.noreply.github.com>
2020-09-30 12:17:45 +0900

    third commit

--- update.py
+++ update.py
@@ -106,5 +106,13 @@ data24=sum(data24)*300
 
 data=data1+data2+data3+data4+data5+data6+data7+data8+data9+data10+data11+data12+data13+data14+data15+data16+data17+data18+data19+data20+data21+data22+data23+data24
 
-with open("/home/tobiuo0203/static/data.json", mode='w') as f:
-    f.write(json.dumps([data]))
+with open("/home/tobiuo0203/static/data.json", mode='ab+') as f:
+    f.seek(-1, 2)
+    f.truncate()
+    f.write(','.encode())
+    f.write(json.dumps(data).encode())
+    f.write(']'.encode())
+
+
+
+
